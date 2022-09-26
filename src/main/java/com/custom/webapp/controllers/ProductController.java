81182296fa3ed81c5c5dc46c57041d92b495bea5
Ahmad <amirizalrahmat@gmail.com>
2022-09-26 19:58:53 +0800

    Class to handle request of template page

---
+++ src/main/java/com/custom/webapp/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package com.custom.webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+	@GetMapping("/")
+	public String showMainPage() {
+		return "index";
+	}
+}
